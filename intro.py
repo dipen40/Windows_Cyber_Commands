af160d331a0c4177238e7f29e5a96faca9f5f2d8
GigaHertzLegacy-SpiderX <abirali20205@gmail.com>
2022-07-09 12:30:07 +0600

    version-1

---
+++ intro.py
@@ -0,0 +1,18 @@
+
+def intro1():
+    print("""\033[0;37;40m
+        ~ Cyber Security Handy Commands For Windows System (cmd/powershell)
+
+                1. Computer's IP Settng ( ipconfig )
+                2. Hit Website IP Address ( Ping )
+                3. IP Tracer ( tracert )
+                4. Deep IP Tracing ( Pathping )
+                5. ARP Table ( arp -a )
+                6. DNS Resolver ( nslookup )
+                7. Manipulate Network Routing Table ( route print )
+                8. All Open Connections ( netstat -an )
+                9. Get detailed TCP/IP info ( ipconfig/all ) 
+               10. Windows Auto-Repair Tool ( sfc /scannow )
+                            ~ 404 ( exit ) ~
+                       ~ 999 ( See this Menu ) ~
+    """)
